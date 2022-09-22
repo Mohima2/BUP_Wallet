db25fabb70dfea4de103282619a6eb3ceb40d4f0
Mohima2 <89738019+Mohima2@users.noreply.github.com>
2022-09-22 23:32:42 +0600

    Add files via upload

---
+++ header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BUP E-Wallet</title>
+    
+</head>
+
+<body>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" href="first.css">
+    <nav>
+        <div class="wrapper">
+            <a href="index.php"><img src="img/download.png" alt="E_Wallet LOGO"></a>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="index.php">About Us</a></li>
+                <li><a href="index.php">Terms & Condition</a></li>
+                <li><a href="index.php">More</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                            <li><a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-log-in"></span> Login / Sign Up <span class="caret"></span></a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="#">Login</a></li>
+                                    <li><a href="#">Sign Up</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+        </div>
+    </nav>
+
+    <div class="wrapper">
