e5c0d8a8a38d42ed5d3dd7d8649338bd55ac8a75
Mohima2 <89738019+Mohima2@users.noreply.github.com>
2022-10-08 11:30:06 +0600

    Update login.php
    Updated Login Page

--- login.php
+++ login.php
@@ -1,20 +1,58 @@
-<?php
-   include_once 'header.php';
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login_Registration</title>
+    <link rel="stylesheet" href="login.css">
+</head>
+<body>
+    <div class="container">
+        <div class="card">
+            <div class="inner-box" id="card">
+             
+                <div class="card-front">
+                    <h2>LOGIN</h2>
+                    <form action="log2.php" method="POST">
+                        <input type="text" class="input-box" placeholder="User ID" name="username" required>
+                        <input type="password" class="input-box" placeholder="Password" name="password" required>
+                        <button type="submit" class="submit-btn">Submit</button>
+                        <input type="checkbox"><span>Remember Me</span>
 
-<section class="signup-form">
-    <h2>Log In</h2>
-    <div class="signup-form" id="card">
-    <form action="login.inc.php" method="post">
-          <input type="text" class="input-box" placeholder="User-name/ Email" required>    
-          <input type="password" class="input-box" placeholder="Password" required>
-          <button type="submit" class="submit-btn">Log In</button>                       
-          <input type="checkbox"><span>Remember Me</span>                   
-    </form>
+                    </form>
+                    <button type="button" class="btn" onclick="openRegister()">I'm new here</button>
+                    <a href="">Forget Password</a>
+                </div>
+            
+                <div class="card-back">
+                    <h2>Register</h2>
+                    <form action="reg.php" method="POST">
+                        <input type="text" class="input-box" placeholder="Student Id" name="uid" required>
+                        <input type="email" class="input-box" placeholder="Email Id" name="email" required>
+                        <input type="password" class="input-box" placeholder="Password" name="pwd" required>
+                        <button type="submit" class="submit-btn" name="submit">Submit</button>
+                        <input type="checkbox"><span>Remember Me</span>
+                    </form>
+                    <button type="button" class="btn" onclick="openLogin()">Already have an account?</button>
+                    
+               
+                </div>  
+            </div>
+        </div>
     </div>
-</section>
+    <script>
+        var card = document.getElementById("card");
 
+        function openRegister(){
+            card.style.transform="rotateY(-180deg)";
+        }
 
-<?php
-   include_once 'footer.php';
-?>
+        function openLogin(){
+            card.style.transform="rotateY(0deg)";
+        }
+
+    </script>
+    
+</body>
+</html>
