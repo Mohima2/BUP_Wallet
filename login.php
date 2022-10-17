fad571722c7d032a135d04729fa58a5246adc44f
Mohima2 <89738019+Mohima2@users.noreply.github.com>
2022-10-17 22:37:28 +0600

    demo...for checking

--- login.php
+++ login.php
@@ -1,58 +1,84 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+	
+include("connect.php");
+
+if(isset($_POST['login'])) {
+	$sql = mysqli_query($conn,
+	"SELECT * FROM REGISTER WHERE username='"
+	. $_POST["username"] . "' AND
+	password='" . $_POST["pwd"] . "' ");
+
+	$num = mysqli_num_rows($sql);
+
+	if($num > 0) {
+		$row = mysqli_fetch_array($sql);
+		header("location:check.php");
+		exit();
+	}
+	else {
+?>
+<hr>
+<font color="red"><b>
+		<h3>Sorry Invalid Username and Password<br>
+			Please Enter Correct Credentials</br></h3>
+	</b>
+</font>
+<hr>
+
+<?php
+	}
+}
+?>
+<html>
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login_Registration</title>
-    <link rel="stylesheet" href="login.css">
+	<style>
+		th {
+			text-align: left;
+		}
+
+		td {
+			text-align: center;
+		}
+
+		a {
+			text-decoration: none;
+		}
+	</style>
 </head>
+
 <body>
-    <div class="container">
-        <div class="card">
-            <div class="inner-box" id="card">
-             
-                <div class="card-front">
-                    <h2>LOGIN</h2>
-                    <form action="log2.php" method="POST">
-                        <input type="text" class="input-box" placeholder="User ID" name="username" required>
-                        <input type="password" class="input-box" placeholder="Password" name="password" required>
-                        <button type="submit" class="submit-btn">Submit</button>
-                        <input type="checkbox"><span>Remember Me</span>
-
-                    </form>
-                    <button type="button" class="btn" onclick="openRegister()">I'm new here</button>
-                    <a href="">Forget Password</a>
-                </div>
-            
-                <div class="card-back">
-                    <h2>Register</h2>
-                    <form action="reg.php" method="POST">
-                        <input type="text" class="input-box" placeholder="Student Id" name="uid" required>
-                        <input type="email" class="input-box" placeholder="Email Id" name="email" required>
-                        <input type="password" class="input-box" placeholder="Password" name="pwd" required>
-                        <button type="submit" class="submit-btn" name="submit">Submit</button>
-                        <input type="checkbox"><span>Remember Me</span>
-                    </form>
-                    <button type="button" class="btn" onclick="openLogin()">Already have an account?</button>
-                    
-               
-                </div>  
-            </div>
-        </div>
-    </div>
-    <script>
-        var card = document.getElementById("card");
-
-        function openRegister(){
-            card.style.transform="rotateY(-180deg)";
-        }
-
-        function openLogin(){
-            card.style.transform="rotateY(0deg)";
-        }
-
-    </script>
-    
+	<form method="post" action="login.php">
+		<fieldset>
+			<legend align="center">
+				<h1 align="center">Login</h1>
+			</legend>
+			<table width="50%" border="1"
+				align="center" style="border:thick;">
+				<tr>
+					<th height="40"><label for="username">
+						Username</label>
+					</th>
+					<td><input type="text" name="username"
+							id="username" required>
+						</td>
+				</tr>
+				<tr>
+					<th height="40"><label for="pwd">
+						Password
+					</label>
+					</th>
+					<td><input type="password"
+						name="pwd" id="pwd" required></td>
+				</tr>
+				<tr>
+					<td colspan="2" height="40"><input
+						type="submit" name="login"
+						value="Login"></td>
+				</tr>
+			</table>
+		</fieldset>
+	</form>
 </body>
+
 </html>
