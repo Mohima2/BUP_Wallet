cee710b5c2ac0a4cfce79ba86efa2d013b6a7f22
Mohima2 <89738019+Mohima2@users.noreply.github.com>
2022-10-21 19:40:43 +0600

    Latest registration form

--- index.php
+++ index.php
@@ -4,130 +4,94 @@ include("connect.php");
 
 if(isset($_POST['submit'])) {
 	$query = mysqli_query($conn,
-	"INSERT INTO REGISTER SET firstname='"
-	. $_POST["firstname"] . "' ,lastname ='"
-	. $_POST["lastname"] . "' ,username ='"
-	. $_POST["username"] . "' ,year	 ='"
-	. $_POST["year"] . "'	 ,mobile	 ='"
-	. $_POST["mob"] . "'	 ,password ='"
+	"INSERT INTO reg SET username ='"
+	. $_POST["username"] . "' ,usertype	 ='"
+	. $_POST["usertype"] . "'	 ,email	 ='"
+	. $_POST["email"] . "'	 ,password ='"
 	. $_POST["pwd"] . "'	 ");
 	
 ?>
-<script>
-	alert('You Registered Successfully, Login now');
-</script>
+
 <?php
 }
 ?>
-<html>
 
+<!DOCTYPE html>
+<html lang="en">
 <head>
-	<meta charset="utf-8">
-	<title>Register Page</title>
-	<style>
-		th {
-			text-align: left;
-		}
-
-		td {
-			text-align: center;
-		}
-
-		a {
-			text-decoration: none;
-		}
-	</style>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login_Registration</title>
+    <link rel="stylesheet" href="login.css">
 </head>
-
 <body>
-	<a href="login.php"
-		style="font-size:30px; float:right;">
-		Login
-	</a>
-	<form method="post" action="index.php" name="frm1">
+    <div class="container">
+        <div class="card">
+            <div class="inner-box" id="card">
+             
+                <div class="card-front">
+                    <h2>LOGIN</h2>
+                    <form action="login.php" method="POST">
+                        <input type="text" class="input-box" placeholder="User ID" name="username" required>
+                        <input type="password" class="input-box" placeholder="Password" name="pwd" required>
+                        <button type="submit" class="submit-btn" name="login">Submit</button>
+                        <input type="checkbox"><span>Remember Me</span>
 
-		<fieldset>
-			<legend align="center">
-				<h1>Register Here</h1>
-			</legend>
-			<table align="center" border="1"
-				width="40%" style="border:thick;">
-				<tr>
-					<th height="40"><label for="firstname">
-							First Name</label>
-					</th>
-					<td><input type="text"
-						name="firstname"
-						id="firstname" required>
-					</td>
-				</tr>
-				<tr>
-					<th height="40"><label for="lastname">
-							Last Name</label>
-					</th>
-					<td><input type="text"
-							name="lastname"
-							id="lastname" required>
-					</td>
-				</tr>
-				<tr>
-					<th height="40"><label for="username">
-							Username</label>
-					</th>
-					<td><input type="text"
-						name="username"
-						id="username" required>
-					</td>
-				</tr>
-				<tr>
+                    </form>
+                    <button type="button" class="btn" onclick="openRegister()">I'm new here</button>
+                    <a href="">Forget Password</a>
+                </div>
+            
+                <div class="card-back">
+                    <h2>Register</h2>
+                    <form action="index.php" method="POST">
+                        <input type="text" class="input-box" placeholder="User Id" name="username" required>
+                        <table>
+                        <tr>
 					<th height="40">
-						<label for="year">Year</label>
+						<label for="usertype">User Type</label>
 					</th>
-					<td><select name="year"
-						id="year" required>
+					<td><select name="usertype"
+						id="usertype" required>
 							<option value="">
-								Choose Year
+								Select User
 							</option>
-							<option value="First Year">
-								First Year
+							<option value="user">
+								Student
 							</option>
-							<option value="Second Year">
-								Second Year
-							</option>
-							<option value="Third Year">
-								Third Year
-							</option>
-							<option value="Fourth Year">
-								Fourth Year
+							<option value="admin">
+								Admin
 							</option>
 						</select>
 					</td>
 				</tr>
-				<tr>
-					<th height="40">
-						<label for="mob">Mob.No.</label>
-					</th>
-					<td><input type="tel" name="mob"
-							id="mob" required>
-					</td>
-				</tr>
-				<tr>
-					<th height="40">
-						<label for="pwd">Password</label>
-					</th>
-					<td><input type="password"
-						name="pwd" id="pwd" required>
-					</td>
-				</tr>
-				<tr>
-					<td height="40" colspan="2"><input
-						type="submit" name="submit"
-						value="Register">
-					</td>
-				</tr>
-			</table>
-		</fieldset>
-	</form>
-</body>
+                        </table>
+                         <input type="email" class="input-box" placeholder="Email Id" name="email" required>
+                        <input type="password" class="input-box" placeholder="Password" name="pwd" required>
+                        <button type="submit" class="submit-btn" name="submit">Submit</button>
+                        <input type="checkbox"><span>Remember Me</span>
+                    </form>
+                    <button type="button" class="btn" onclick="openLogin()">Already have an account?</button>
+                    
+               
+                </div>  
+            </div>
+        </div>
+    </div>
+    <script>
+        var card = document.getElementById("card");
 
+        function openRegister(){
+            card.style.transform="rotateY(-180deg)";
+        }
+
+        function openLogin(){
+            card.style.transform="rotateY(0deg)";
+        }
+
+    </script>
+    
+</body>
 </html>
+
